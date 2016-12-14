f2fceef8e069e5bf196229d7ed9e0c2c2123c639
Stephan Trube <StephanTrube@gmail.com>
2016-12-14 15:49:55 +0100

    trying to get jQuery.click() working

--- public/javascript/fourwinning.js
+++ public/javascript/fourwinning.js
@@ -1,6 +1,12 @@
-function throwChip(elementID)
-{
-    $('#Intro')
+console.log("fourwinning.js loaded!");
 
-    console.log("Element ID: ", elementID);
+function throwChip(event) {
+    console.log("click detected!");
+    console.log("event info: ", event);
 }
+
+
+$('#throwChip').click(function(event) {
+    console.log("click detected!");
+    console.log("event info: ", event);
+});
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,15 @@
 // is called when index.html finishes loading it's body
-console.log("main.js");
+console.log("main.js loaded!");
 function start() {
     alert("Geht doch!");
-    
 }
+
+$(document).ready(function() {
+
+    console.log("document ready");
+
+    $('#throwChip').click(function(event) {
+        console.log("throwChip event");
+        throwChip(event);
+    });
+});
