e14c533ea369eff6b71aeea2a015ab20db0ea409
Michi <michi28081993@web.de>
2016-12-21 15:55:25 +0100

    Prework for sockets included

--- public/javascript/fourwinning.js
+++ public/javascript/fourwinning.js
@@ -1,3 +1,13 @@
+function openSocket(){
+    socket = new WebSocket('http://localhost:9000/ws');
+    socket.onopen = function(){
+        socket.send('hello');
+    }
+    socket.onmessage = function(s){
+        alert('got reply: ' + s)
+    }
+}
+
 function throwChip(elementID)
 {
     console.log("Element ID: ", elementID);
