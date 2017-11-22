bc0210b8f3b45f8a3324a9656ff42d1e8965ecde
Michi <michi28081993@web.de>
2017-11-22 15:22:41 +0100

    sock.js excluded

--- public/javascript/sock.js
+++ public/javascript/sock.js
@@ -1,6 +1,9 @@
 "use strict";   // https://www.w3schools.com/js/js_strict.asp
 
 var socket;
+var cols = 7;
+var rows = 6;
+var counter = 0;
 
 $(function() {
     // add a click handler to the button
@@ -99,3 +102,27 @@ function buildNewGameField(msg){
     }
     document.getElementById("gamefield").innerHTML = innerhtml;
 }
+
+function buildGame(){
+    document.getElementById('toggle').style.visibility = 'hidden';
+    var innerhtml = "";
+    for (var i=-1; i < rows; i++){
+        innerhtml += '<tr align="center">';
+        for (var j=0; j < cols; j++){
+            var s = String(i) + "," + String(j);
+            if (i === -1){
+                innerhtml += '<td><img id=' + j + ' class="img-responsive throwChip" onclick="send(id)" src="/assets/images/pfeil.gif"/></td>';
+                
+            } else {
+                innerhtml += '<td><img id=' + s + ' class="img-responsive throwChip" src="/assets/images/leer.gif"/></td>';
+            }
+        }
+        innerhtml += "</tr>";
+    }
+    document.getElementById("gamefield").innerHTML = innerhtml;
+}
+
+function makeString(index, target) {
+    //var buildstring = "@routes.Assets.at("images/rot.gif")";
+    //return '<td><img id=' + index + ' class="img-responsive throwChip" src="' + buildstring + '"/></td>';
+} 
