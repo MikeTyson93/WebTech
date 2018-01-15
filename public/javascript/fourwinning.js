b76469cff3d0830ebbe25d612a3f5ed07a6e42de
Stephan Trube <StephanTrube@gmail.com>
2018-01-15 16:04:28 +0100

    cleanup: removed duplicate script sources and clearified js entrypoint for fourwinning

---
+++ public/javascript/fourwinning.js
@@ -0,0 +1,8 @@
+"use strict";   // https://www.w3schools.com/js/js_strict.asp
+
+// this jQuery is called once the page has finished loading
+$(function() {
+    console.log("fourwinning.js: page finished loading");
+
+    connectWebSocket();
+});
